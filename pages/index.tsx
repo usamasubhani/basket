2e71e051cdcbd29abbbfaf30c6c81c1fe8b77094
Usama Subhani <reachusama@protonmail.com>
2020-08-24 11:49:15 +0500

    Wrap components in Provider

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,22 @@
 import React from "react"
 import { Container, Box } from "@material-ui/core"
-
+import { Provider } from "react-redux"
 import { Basket } from "../components/Basket"
 import { Product } from "../components/Product"
+import { store } from "../store"
 
 const ShoppingBasket = () => {
   return (
-    <Container maxWidth="md">
-      <Box mt={5} mb={5}>
-        <Product />
-      </Box>
-      <Box mt={5} mb={5}>
-        <Basket />
-      </Box>
-    </Container>
+    <Provider store={store}>
+      <Container maxWidth="md">
+        <Box mt={5} mb={5}>
+          <Product />
+        </Box>
+        <Box mt={5} mb={5}>
+          <Basket />
+        </Box>
+      </Container>
+    </Provider>
   )
 }
 
