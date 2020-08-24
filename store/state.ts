158e6d7de21fbaf5fdadc5e2f3e6189533035166
Usama Subhani <reachusama@protonmail.com>
2020-08-24 12:33:19 +0500

    Add more products

--- store/state.ts
+++ store/state.ts
@@ -2,14 +2,14 @@ import { ProductItem } from '../global'
 
 const INITIAL_STATE : ProductItem[] = [
     {
-        id: '123',
+        id: '1',
         title: 'Black Seude',
         description: 'Welcome to the dark side',
         imageUrl: '/img/1.jpg',
         price: 399
     },
     {
-        id: '456',
+        id: '2',
         title: 'Mule',
         description:
           'Back to the future',
@@ -17,12 +17,33 @@ const INITIAL_STATE : ProductItem[] = [
         price: 499
     },
     {
-        id: '789',
+        id: '3',
         title: 'Chelsea',
         description: 'God bless Carl Marx',
         imageUrl: '/img/8.jpg',
         price: 799
-    }
+    },
+    {
+        id: '4',
+        title: 'Desert Flats',
+        description: 'Go & kill snakes',
+        imageUrl: '/img/2.jpg',
+        price: 398
+    },
+    {
+        id: '5',
+        title: 'Angel',
+        description: 'Hitler did nothing wrong',
+        imageUrl: '/img/5.jpg',
+        price: 999
+    },
+    {
+        id: '6',
+        title: 'Desert',
+        description: 'Git Gud',
+        imageUrl: '/img/7.jpg',
+        price: 999
+    },
 ]
 
 export { INITIAL_STATE }
